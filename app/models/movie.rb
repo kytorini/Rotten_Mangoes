01b7153ee649f25f330469218904d3a27e1f2e6a
kytorini <khung@me.com>
2016-03-31 20:35:27 +0000

    Generate review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,22 +1,19 @@
 class Movie < ActiveRecord::Base
 
+  has_many :reviews
+
   validates :title,
-  presence: true
-
+    presence: true
   validates :director,
-  presence: true
-
+    presence: true
   validates :runtime_in_minutes,
-  numericality: { only_integer: true }
-
+    numericality: { only_integer: true }
   validates :description,
-  presence: true
-
+    presence: true
   validates :poster_image_url,
-  presence: true
-
+    presence: true
   validates :release_date,
-  presence: true
+    presence: true
 
   validate :release_date_is_in_the_past
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :reviews
 
-has_secure_password
- 
-validates :email,
- presence: true
-
-validates :firstname,
- presence: true
-
-validates :lastname,
- presence: true
-
-validates :password,
- length: { in: 6..20 }, on: :create
+  has_secure_password
+   
+  validates :email,
+   presence: true
+  validates :firstname,
+   presence: true
+  validates :lastname,
+   presence: true
+  validates :password,
+   length: { in: 6..20 }, on: :create
  
 end
