858728a4a173ae0345b88c07889fd183d9156401
kytorini <khung@me.com>
2016-03-31 19:47:36 +0000

    Add routes for user, Add User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RottenMangoes::Application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   resources :movies
+  resources :users, only: [:new, :create]
   # root to: 'movies#index'
 end
